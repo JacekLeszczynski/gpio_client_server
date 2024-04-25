8ae5d0fec796507fdd48b3f2b16db00403dabc08
Jacek Leszczyński <poczta@pistudio.duckdns.org>
2024-04-25 12:16:31 +0200

    Dodanie kodu obsługującego wątek

--- service/daemonunit1.pas
+++ service/daemonunit1.pas
@@ -5,14 +5,21 @@ unit DaemonUnit1;
 interface
 
 uses
-  Classes, SysUtils, DaemonApp;
+  Classes, SysUtils, DaemonApp, Main;
 
 type
+
+  { TDaemon1 }
+
   TDaemon1 = class(TDaemon)
+    procedure DataModuleContinue(Sender: TCustomDaemon; var OK: Boolean);
+    procedure DataModulePause(Sender: TCustomDaemon; var OK: Boolean);
+    procedure DataModuleShutDown(Sender: TCustomDaemon);
+    procedure DataModuleStart(Sender: TCustomDaemon; var OK: Boolean);
+    procedure DataModuleStop(Sender: TCustomDaemon; var OK: Boolean);
   private
-
+    FDaemonWorkerThread: TDaemonWorkerThread;
   public
-
   end;
 
 var
@@ -26,6 +33,44 @@ begin
 end;
 
 {$R *.lfm}
+
+{ TDaemon1 }
+
+procedure TDaemon1.DataModuleContinue(Sender: TCustomDaemon; var OK: Boolean);
+begin
+  FDaemonWorkerThread.Resume;
+  OK:=true;
+end;
+
+procedure TDaemon1.DataModulePause(Sender: TCustomDaemon; var OK: Boolean);
+begin
+  FDaemonWorkerThread.Suspend;
+  OK:=true;
+end;
+
+procedure TDaemon1.DataModuleShutDown(Sender: TCustomDaemon);
+begin
+  self.Stop;
+end;
+
+procedure TDaemon1.DataModuleStart(Sender: TCustomDaemon; var OK: Boolean);
+begin
+  FDaemonWorkerThread:=TDaemonWorkerThread.Create;
+  FDaemonWorkerThread.FreeOnTerminate:=false;
+  FDaemonWorkerThread.Start;
+  OK:=true;
+end;
+
+procedure TDaemon1.DataModuleStop(Sender: TCustomDaemon; var OK: Boolean);
+begin
+  if assigned(FDaemonWorkerThread) then
+  begin
+    FDaemonWorkerThread.Terminate;
+    FDaemonWorkerThread.WaitFor;
+    FreeAndNil(FDaemonWorkerThread);
+  end;
+  OK:=true;
+end;
 
 
 initialization
